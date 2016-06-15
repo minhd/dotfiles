a58e204aae7bccf4db4435195f596cae1de50483
Minh Duc Nguyen <dekarvn@gmail.com>
2016-06-15 18:13:25 +1000

    Added sublime configuration

--- replace.sh
+++ replace.sh
@@ -7,3 +7,7 @@ cp ~/.bash_prompt .
 cp ~/.gitconfig .
 cp ~/.brew .
 cp ~/.bash_profile .
+cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings sublime/
+cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings sublime/
+cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap sublime/
+
