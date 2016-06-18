8c5331943469a7f9a78dc40a688ca209568e3333
Minh Duc Nguyen <dekarvn@gmail.com>
2016-06-18 17:53:15 +1000

    Added support to backup Sublime 3 settings and keymap

--- replace.sh
+++ replace.sh
@@ -4,3 +4,7 @@ for file in ~/.{vimrc,functions,aliases,bash_prompt,gitconfig,bash_profile}; do
 	cp $file .	
 done
 unset file
+
+cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings sublime/
+cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap sublime/
+cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings sublime/
